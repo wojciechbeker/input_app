e37fcc6d3228cb31e60d010ef3b4154623d19cda
wojciechbeker <wojciechbeker@users.noreply.github.com>
2022-08-10 16:57:09 +0200

    Corrected PEP8

--- test/test_input_handler.py
+++ test/test_input_handler.py
@@ -6,12 +6,14 @@ class TestInputHandler:
     def test_email_acceptable(self):
         error_code = validate_input('a@b.c')
         if error_code:
-            pytest.fail(f'Correct email address was rejected with error code :{error_code}')
+            pytest.fail(f'Correct email address was rejected with error code :'
+                        f'{error_code}')
 
     def test_email_upper_cases(self):
         error_code = validate_input('a@b.c')
         if error_code:
-            pytest.fail(f'Correct email address was rejected with error code :{error_code}')
+            pytest.fail(f'Correct email address was rejected with error code :'
+                        f'{error_code}')
 
     def test_email_too_short(self):
         pass
