4df4ae847fbe942f24bbabcce3fad932d474cbb0
wojciechbeker <wojciechbeker@users.noreply.github.com>
2022-08-10 16:51:01 +0200

    Initial commit for input handler

---
+++ input_app/input_handler/input_handler.py
@@ -0,0 +1,3 @@
+def validate_input(input_string):
+    error_code = 0
+    return error_code
---
+++ test/test_input_handler.py
@@ -0,0 +1,29 @@
+import pytest
+from input_app.input_handler.input_handler import validate_input
+
+
+class TestInputHandler:
+    def test_email_acceptable(self):
+        error_code = validate_input('a@b.c')
+        if error_code:
+            pytest.fail(f'Correct email address was rejected with error code :{error_code}')
+
+    def test_email_upper_cases(self):
+        error_code = validate_input('a@b.c')
+        if error_code:
+            pytest.fail(f'Correct email address was rejected with error code :{error_code}')
+
+    def test_email_too_short(self):
+        pass
+
+    def test_email_format_incorrect(self):
+        pass
+
+    def test_email_too_long(self):
+        pass
+
+    def test_email_forbidden_characters(self):
+        pass
+
+    def test_email_whitespaces(self):
+        pass
